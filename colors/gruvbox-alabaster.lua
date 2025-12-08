46a6337c5a5937de3b66def24ec08895484191e6
Ohjne <er@le.com>
2025-12-07 23:40:45 -0600

    bruh

--- colors/gruvbox-alabaster.lua
+++ colors/gruvbox-alabaster.lua
@@ -381,12 +381,12 @@ if vim.o.background == "dark" then
 		NoiceCmdlinePopupBorderSearch = { link = "GruvboxAlabasterYellow" },
 		NoiceCmdlineIconSearch = { link = "GruvboxAlabasterYellow" },
 		-- Ministatusline
-		MiniStatuslineModeNormal = { fg = #282828, bg = "#ebdbb2", nocombine = true },
-		MiniStatuslineModeCommand = { fg = #282828, bg = "#fabd2f", nocombine = true },
-		MiniStatuslineModeReplace = { fg = #282828, bg = "#fb4934", nocombine = true },
-		MiniStatuslineModeInsert = { fg = #282828, bg = "#83a598", nocombine = true },
-		MiniStatuslineModeVisual = { fg = #282828, bg = "#b8bb26", nocombine = true },
-		MiniStatuslineModeOther = { fg = #282828, bg = "#8ec07c", nocombine = true },
+		MiniStatuslineModeNormal = { fg = "#282828", bg = "#ebdbb2", nocombine = true },
+		MiniStatuslineModeCommand = { fg = "#282828", bg = "#fabd2f", nocombine = true },
+		MiniStatuslineModeReplace = { fg = "#282828", bg = "#fb4934", nocombine = true },
+		MiniStatuslineModeInsert = { fg = "#282828", bg = "#83a598", nocombine = true },
+		MiniStatuslineModeVisual = { fg = "#282828", bg = "#b8bb26", nocombine = true },
+		MiniStatuslineModeOther = { fg = "#282828", bg = "#8ec07c", nocombine = true },
 		-- Languages
 		--- asm
 		asmDirective = { fg = dim_comment },
