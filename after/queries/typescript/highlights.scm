80d79f9ecc85ef29f5164b51c1e7b0ced09c27d9
Ohjne <er@le.com>
2025-12-22 14:19:41 -0600

    now?

--- after/queries/typescript/highlights.scm
+++ after/queries/typescript/highlights.scm
@@ -31,3 +31,10 @@
   "]"
 ] @punctuation.bracket
 
+(named_imports
+  "{" @GruvboxAlabasterDelimiter
+  "}" @GruvboxAlabasterDelimiter)
+
+(object
+  "{" @GruvboxAlabasterDelimiter
+  "}" @GruvboxAlabasterDelimiter)
